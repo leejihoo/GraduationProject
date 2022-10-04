255a95d219f810560e7251fafc8b53e0eb12cad0
leejihoo <ajtwlsdl33@gmail.com>
2022-10-04 15:09:41 +0900

    Chain Safe Game SDK import
    이더리움과의 통신에 필요한 기능들이 구현된 SDK를 추가

---
+++ Assets/Web3Unity/Prefabs/ERC1155/ImportNFTTextureExample.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ImportNFTTextureExample : MonoBehaviour
+{
+    public class Response {
+        public string image;
+    }
+    
+
+    async void Start()
+    {
+        string chain = "ethereum";
+        string network = "goerli";
+        string contract = "0x2c1867BC3026178A47a677513746DCc6822A137A";
+        string tokenId = "0x01559ae4021a8aad9226aef5beee2a0d8ba61eaac07e3bc71d2d9e9a802a216f";
+
+        // fetch uri from chain
+        string uri = await ERC1155.URI(chain, network, contract, tokenId);
+        print("uri: " + uri);
+
+        // fetch json from uri
+        UnityWebRequest webRequest = UnityWebRequest.Get(uri);
+        await webRequest.SendWebRequest();
+        Response data = JsonUtility.FromJson<Response>(System.Text.Encoding.UTF8.GetString(webRequest.downloadHandler.data));
+
+        // parse json to get image uri
+        string imageUri = data.image;
+        print("imageUri: " + imageUri);
+        if (imageUri.StartsWith("ipfs://"))
+        {
+            imageUri = imageUri.Replace("ipfs://", "https://ipfs.io/ipfs/");
+        }
+        Debug.Log("Revised URI: " + imageUri);
+        // fetch image and display in game
+        UnityWebRequest textureRequest = UnityWebRequestTexture.GetTexture(imageUri);
+        await textureRequest.SendWebRequest();
+        gameObject.GetComponent<Renderer>().material.mainTexture = ((DownloadHandlerTexture)textureRequest.downloadHandler).texture;
+    }
+}
---
+++ Assets/Web3Unity/Scripts/Library/WebGL/Web3GL.cs
@@ -0,0 +1,206 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using UnityEngine;
+
+#if UNITY_WEBGL
+public class Web3GL
+{
+    [DllImport("__Internal")]
+    private static extern void SendContractJs(string method, string abi, string contract, string args, string value, string gasLimit, string gasPrice);
+
+    [DllImport("__Internal")]
+    private static extern string SendContractResponse();
+
+    [DllImport("__Internal")]
+    private static extern void SetContractResponse(string value);
+
+    [DllImport("__Internal")]
+    private static extern void CallContractJs(string method, string abi, string contract, string args);
+
+    [DllImport("__Internal")]
+    private static extern void ResetCallContractResponse();
+
+    [DllImport("__Internal")]
+    private static extern string CallContractResponse();
+
+    [DllImport("__Internal")]
+    private static extern string CallContractError();
+
+    [DllImport("__Internal")]
+    private static extern void SendTransactionJs(string to, string value, string gasLimit, string gasPrice);
+    
+    [DllImport("__Internal")]
+    private static extern void SendTransactionJsData(string to, string value, string gasPrice, string gasLimit, string data);
+
+    [DllImport("__Internal")]
+    private static extern string SendTransactionResponse();
+
+    [DllImport("__Internal")]
+    private static extern void SetTransactionResponse(string value);
+    
+    [DllImport("__Internal")]
+    private static extern void SetTransactionResponseData(string value);
+
+    [DllImport("__Internal")]
+    private static extern void SignMessage(string value);
+    
+    
+    [DllImport("__Internal")]
+    private static extern void HashMessage(string value);
+
+    [DllImport("__Internal")]
+    private static extern string SignMessageResponse();
+    
+    [DllImport("__Internal")]
+    private static extern string HashMessageResponse();
+
+    [DllImport("__Internal")]
+    private static extern void SetSignMessageResponse(string value);
+    
+    [DllImport("__Internal")]
+    private static extern void SetHashMessageResponse(string value);
+
+    [DllImport("__Internal")]
+    private static extern int GetNetwork();
+
+    async public static Task<string> CallContract(string _method, string _abi, string _contract, string _args, float _waitSeconds = 0.1f)
+    {
+        ResetCallContractResponse();
+        CallContractJs(_method, _abi, _contract, _args);
+        string response = CallContractResponse();
+        string error = CallContractError();
+        while (response == "" && error == "")
+        {
+            await new WaitForSeconds(_waitSeconds);
+            response = CallContractResponse();
+            error = CallContractError();
+        }
+        ResetCallContractResponse();
+        if (error.Length > 0)
+        {
+            throw new Exception(error);
+        } else
+        {
+            return response;
+        }
+    }
+
+    // this function will create a metamask tx for user to confirm.
+    async public static Task<string> SendContract(string _method, string _abi, string _contract, string _args, string _value, string _gasLimit = "", string _gasPrice = "")
+    {
+        // Set response to empty
+        SetContractResponse("");
+        SendContractJs(_method, _abi, _contract, _args, _value, _gasLimit, _gasPrice);
+        string response = SendContractResponse();
+        while (response == "")
+        {
+            await new WaitForSeconds(1f);
+            response = SendContractResponse();
+        }
+        SetContractResponse("");
+        // check if user submmited or user rejected
+        if (response.Length == 66) 
+        {
+            return response;
+        } 
+        else 
+        {
+            throw new Exception(response);
+        }
+    }
+
+    async public static Task<string> SendTransaction(string _to, string _value, string _gasLimit = "", string _gasPrice = "")
+    {
+        // Set response to empty
+        SetTransactionResponse("");
+        SendTransactionJs(_to, _value, _gasLimit, _gasPrice);
+        string response = SendTransactionResponse();
+        while (response == "")
+        {
+            await new WaitForSeconds(1f);
+            response = SendTransactionResponse();
+        }
+        SetTransactionResponse("");
+        // check if user submmited or user rejected
+        if (response.Length == 66) 
+        {
+            return response;
+        } 
+        else 
+        {
+            throw new Exception(response);
+        }
+    }
+    
+    async public static Task<string> SendTransactionData(string _to, string _value,  string _gasPrice = "",string _gasLimit = "", string _data = "")
+    {
+        // Set response to empty
+        SetTransactionResponse("");
+        SendTransactionJsData(_to, _value, _gasPrice,_gasLimit,_data);
+        string response = SendTransactionResponse();
+        while (response == "")
+        {
+            await new WaitForSeconds(1f);
+            response = SendTransactionResponse();
+        }
+        SetTransactionResponse("");
+        // check if user submmited or user rejected
+        if (response.Length == 66) 
+        {
+            return response;
+        } 
+        else 
+        {
+            throw new Exception(response);
+        }
+    }
+
+    async public static Task<string> Sign(string _message)
+    {
+        SignMessage(_message);
+        string response = SignMessageResponse();
+        while (response == "")
+        {
+            await new WaitForSeconds(1f);
+            response = SignMessageResponse();
+        }
+        // Set response to empty
+        SetSignMessageResponse("");
+        // check if user submmited or user rejected
+        if (response.Length == 132)
+        {
+            return response;
+        } 
+        else 
+        {
+            throw new Exception(response);
+        }
+    }
+    
+    async public static Task<string> Sha3(string _message)
+    {
+        HashMessage(_message);
+        SetHashMessageResponse("");
+        try
+        {
+            await new WaitForSeconds(1f);
+            string response = HashMessageResponse();
+            return response;
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+            throw;
+        }
+    }
+
+    public static int Network()
+    {
+        return GetNetwork();
+    }
+
+}
+#endif

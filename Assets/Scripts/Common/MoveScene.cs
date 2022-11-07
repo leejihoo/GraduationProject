deac381d77c032c380c397666b9f7457d1aa4524
leejihoo <ajtwlsdl33@gmail.com>
2022-11-07 16:14:32 +0900

    1차 리팩토링
    startScene & LobbyScene
    gameObject 이름 수정
    스크립트 수정
    - private 변수 _ 붙이기
    - 선언 순서 변경
    async private => async
    - namespace 설정

--- Assets/Scripts/Common/MoveScene.cs
+++ Assets/Scripts/Common/MoveScene.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MoveScene : MonoBehaviour
 {
-    public string SceneName;
+    public string sceneName;
 
     public void MoveSceneTo()
     {
         // 이렇게 안하고 MoveSceneTo에서 바로 string받아도 됨.
         // SceneName을 문자형으로 받는 것은 실수 발생할 수 있으니 Scene번호를 사용하는 것도 괜찮음.
-        SceneManager.LoadScene(SceneName);
+        SceneManager.LoadScene(sceneName);
     }
 }
--- Assets/Web3Unity/Scripts/Scenes/WebLogin.cs
+++ Assets/Web3Unity/Scripts/Scenes/WebLogin.cs
@@ -1,50 +1,44 @@
-﻿using System;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 #if UNITY_WEBGL
-public class WebLogin : MonoBehaviour
+namespace Web3Unity.Scripts.Scenes
 {
-    [DllImport("__Internal")]
-    private static extern void Web3Connect();
-
-    [DllImport("__Internal")]
-    private static extern string ConnectAccount();
-
-    [DllImport("__Internal")]
-    private static extern void SetConnectAccount(string value);
-
-    private int expirationTime;
-    private string account; 
-
-    public void OnLogin()
+    public class WebLogin : MonoBehaviour
     {
-        Web3Connect();
-        OnConnected();
-    }
+        [DllImport("__Internal")]
+        private static extern void Web3Connect();
 
-    async private void OnConnected()
-    {
-        account = ConnectAccount();
-        while (account == "") {
-            await new WaitForSeconds(1f);
-            account = ConnectAccount();
-        };
-        // save account for next scene
-        PlayerPrefs.SetString("Account", account);
-        // reset login message
-        SetConnectAccount("");
-        // load next scene
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+        [DllImport("__Internal")]
+        private static extern string ConnectAccount();
 
-    public void OnSkip()
-    {
-        // burner account for skipped sign in screen
-        PlayerPrefs.SetString("Account", "");
-        // move to next scene
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        [DllImport("__Internal")]
+        private static extern void SetConnectAccount(string value);
+
+        private string _account; 
+
+        public void OnLogin()
+        {
+            Web3Connect();
+            OnConnected();
+        }
+
+        private async void OnConnected()
+        {
+            _account = ConnectAccount();
+            while (_account == "") {
+                await new WaitForSeconds(1f);
+                _account = ConnectAccount();
+            };
+            // save account for next scene
+            PlayerPrefs.SetString("Account", _account);
+            // reset login message
+            SetConnectAccount("");
+            // load next scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
     }
 }
 #endif

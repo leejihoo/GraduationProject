426bd4d82f3db81b71d12cc270cbf2cab64eb77a
leejihoo <ajtwlsdl33@gmail.com>
2022-11-16 18:27:38 +0900

    ERC20 BalanceOf 매개변수 변경
    테스트를 위한 값을 제거

--- Assets/Scripts/Lobby/UpdateTotalCoin.cs
+++ Assets/Scripts/Lobby/UpdateTotalCoin.cs
@@ -11,10 +11,9 @@ namespace Lobby
         async void Update()
         {
             BigInteger balanceOf = await ERC20.BalanceOf(balanceOfGoldCall.Chain, balanceOfGoldCall.Network, 
-                balanceOfGoldCall.ContractAddress, "0xFf7B4b56F3B43D7061Ded409eb5D86341DAD8a95");
-            //PlayerPrefs.GetString("Account")
+                balanceOfGoldCall.ContractAddress,PlayerPrefs.GetString("Account"));
+            // "0xFf7B4b56F3B43D7061Ded409eb5D86341DAD8a95"
             totalGoldText.text = balanceOf.ToString();
         }
-        
     }
 }
